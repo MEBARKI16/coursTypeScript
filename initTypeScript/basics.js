fcf17c207e4e2428d53b6d6d8bf9e5d8f5775a3a
MEBARKI16 <medaminemebarki@gmail.com>
2024-08-27 16:15:44 +0100

    update

---
+++ initTypeScript/basics.js
@@ -0,0 +1,7 @@
+//primitives types
+var age;
+age = 25;
+var nom;
+nom = "amine";
+var grand = true;
+console.log("je m'appel ".concat(nom, " et jai ").concat(age, "ans et je suis ").concat(grand ? "grand" : "petit"));
---
+++ initTypeScript/basics.ts
@@ -0,0 +1,9 @@
+let age: number;
+age = 25;
+
+let nom: string;
+nom = "amine";
+
+let grand: boolean = true;
+
+console.log(`je m'appel ${nom} et jai ${age}ans et je suis ${grand ? "grand" : "petit"}`);
