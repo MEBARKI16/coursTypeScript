6b1fc2046e233a24d0784d481bc4491abd706b0c
MEBARKI16 <medaminemebarki@gmail.com>
2024-08-28 22:20:26 +0100

    function and function types

--- initTypeScript/basics.ts
+++ initTypeScript/basics.ts
@@ -2,9 +2,9 @@
 var age: number;
 age = 25;
 
-var nom : string | number = "amine"
+var nom: string | number = "amine"
 nom = 2
-;
+    ;
 var grand: boolean = true;
 
 console.log(`je m'appel ${nom} et jai ${age}ans et je suis ${grand ? "grand" : "petit"}`);
@@ -27,3 +27,6 @@ var x: person;
 x = { name: "44", prenom: "ff" };
 console.log("mon nom est :" + x.name + " et mon prenom est :" + x.prenom)
 
+function print(x: any): any {
+    console.log(x)
+}
