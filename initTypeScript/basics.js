e05b7c34dd126818a06c72ea17ac0ab831affb1c
MEBARKI16 <medaminemebarki@gmail.com>
2024-08-28 21:18:02 +0100

    working with arrays and objects

--- initTypeScript/basics.js
+++ initTypeScript/basics.js
@@ -1,6 +1,14 @@
+// primitives types
 var age;
 age = 25;
 var nom;
 nom = "amine";
 var grand = true;
 console.log("je m'appel ".concat(nom, " et jai ").concat(age, "ans et je suis ").concat(grand ? "grand" : "petit"));
+// advanced types
+var tab;
+tab = ["a", "b", "c"];
+console.log(tab);
+var x;
+x = { name: "44", prenom: "ff" };
+console.log("mon nom est :" + x.name + " et mon prenom est :" + " x.prenom");
--- initTypeScript/basics.ts
+++ initTypeScript/basics.ts
@@ -1,3 +1,4 @@
+// primitives types
 var age: number;
 age = 25;
 
@@ -7,3 +8,22 @@ nom = "amine";
 var grand: boolean = true;
 
 console.log(`je m'appel ${nom} et jai ${age}ans et je suis ${grand ? "grand" : "petit"}`);
+
+// advanced types
+
+var tab: string[];
+
+tab = ["a", "b", "c"];
+console.log(tab)
+
+
+type person = {
+    name: string,
+    prenom: string
+}
+
+var x: person;
+
+x = { name: "44", prenom: "ff" };
+console.log("mon nom est :" + x.name + " et mon prenom est :" + x.prenom)
+
