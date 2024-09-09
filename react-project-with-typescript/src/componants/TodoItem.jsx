4f5146b1db4e9e9d376dcde50a5bc4e58188fd62
MEBARKI16 <medaminemebarki@gmail.com>
2024-09-09 21:37:53 +0100

    update

---
+++ react-project-with-typescript/src/componants/TodoItem.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const TodoItem: React.FC<{ title: string }> = (props) => {
+    return (
+        <li>{props.title}</li>
+    )
+}
+
+export default TodoItem
--- react-project-with-typescript/src/componants/Todos.tsx
+++ react-project-with-typescript/src/componants/Todos.tsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import Todo from '../models/todo'
+import TodoItem from './TodoItem'
 
 const Todos: React.FC<{ items: Todo[] }> = (props) => {
 
     return (
         <div>
             <ul>
-                {props.items.map((item) => <li key={item.id}>{item.text}</li>)}
+                {props.items.map((item) => <TodoItem key={item.id} title={item.text} />)}
             </ul>
         </div>
     )
